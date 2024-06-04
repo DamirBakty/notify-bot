e4645df48ad53e75bbc444aa9e802360942dcc50
Your Name <you@example.com>
2024-06-04 18:17:37 +0500

    replace bot logger

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import asyncio
 import logging
 
-from environs import Env
-from telegram import Bot
 from telegram.ext import Updater
 
 
@@ -18,7 +16,6 @@ class BotHandler(logging.Handler):
 
     async def send_log_message(self, log_entry):
         try:
-            print(65)
             return await self.updater.bot.send_message(
                 chat_id=self.chat_id,
                 text=log_entry
@@ -28,19 +25,6 @@ class BotHandler(logging.Handler):
             return e
 
 
-async def get_bot_logger(bot, chat_id):
-    updater = Updater(bot=bot, update_queue=None)
-    bot_handler = BotHandler(updater, chat_id)
-    bot_handler.setLevel(logging.DEBUG)
-    bot_handler.setFormatter(logging.Formatter('%(message)s'))
-
-    logger = logging.getLogger('bot_logger')
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(bot_handler)
-
-    return updater
-
-
 async def get_bot_updater(bot):
     updater = Updater(bot=bot, update_queue=None)
     return updater
@@ -48,38 +32,6 @@ async def get_bot_updater(bot):
 
 async def get_bot_handler(updater, chat_id):
     bot_handler = BotHandler(updater, chat_id)
-    bot_handler.setLevel(logging.DEBUG)
+    bot_handler.setLevel(logging.ERROR)
     bot_handler.setFormatter(logging.Formatter('%(message)s'))
     return bot_handler
-
-
-async def start_bot():
-    env = Env()
-    env.read_env()
-    logger = logging.getLogger()
-
-    auth_token = env.str('TOKEN')
-    bot_token = env.str('BOT_TOKEN')
-    chat_id = env.str('CHAT_ID')
-
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    bot = Bot(token=bot_token)
-
-    updater = Updater(bot=bot, update_queue=None)
-    await updater.initialize()
-    bot_handler = BotHandler(updater, chat_id)
-    bot_handler.setLevel(logging.INFO)
-    bot_handler.setFormatter(logging.Formatter('%(message)s'))
-
-    logger.addHandler(bot_handler)
-    await updater.start_polling()
-    message = 'Hello from your Telegram bot!'
-    try:
-        a = 1 / 0
-    except ZeroDivisionError as e:
-        logger.error(e)
-
-    # logger.info("This is an info log message")
-    await updater.stop()
-
-asyncio.run(start_bot())
--- main.py
+++ main.py
@@ -6,104 +6,95 @@ import requests
 from environs import Env
 from telegram import Bot
 
-from bot import get_bot_updater, get_bot_handler, get_bot_logger
+from bot import get_bot_updater, get_bot_handler
 
 
 async def main(bot, chat_id, auth_token):
-
     logger = logging.getLogger()
-    long_polling_url = 'YOUR_LONG_POLLING_URL'
+    updater = await get_bot_updater(bot)
+    bot_handler = await get_bot_handler(updater, chat_id)
+
+    logger.addHandler(bot_handler)
+
     headers = {
         'Authorization': auth_token
     }
 
-    updater = await get_bot_logger(bot, chat_id)
-
-    # Start the updater
-    await updater.start_polling()
+    long_polling_url = 'https://dvmn.org/api/long_polling/'
 
     start_time = time.time()
-
     timestamp = None
+
     params = {}
+    await updater.initialize()
 
-    loop = asyncio.get_running_loop()
-    stop_event = asyncio.Event()
+    while True:
+        await updater.start_polling()
+        try:
+            if timestamp:
+                params['timestamp'] = timestamp
+                logger.debug(f'Started polling with timestamp: {timestamp}')
+            else:
+                logger.debug(f'Started polling without timestamp')
+            response = requests.get(
+                long_polling_url,
+                headers=headers,
+                params=params,
+                timeout=120
+            )
+            response.raise_for_status()
+            logger.debug(f"Polling time = {start_time - time.time()}")
 
-    def shutdown():
-        stop_event.set()
+            review_details = response.json()
+            timestamp = (review_details.get('last_attempt_timestamp')
+                         or review_details.get('timestamp_to_request'))
 
+            new_attempts = review_details.get('new_attempts')
+            if new_attempts:
+                for attempt in new_attempts:
+                    lesson_title = attempt.get('lesson_title')
+                    lesson_url = attempt.get('lesson_url')
+                    is_negative = attempt.get('is_negative')
 
-    try:
-        while not stop_event.is_set():
-            try:
-                a = 1 / 0  # Example error to be handled
-                if timestamp:
-                    params['timestamp'] = timestamp
-                    logger.debug(f'Started polling with timestamp: {timestamp}')
-                else:
-                    logger.debug(f'Started polling without timestamp')
+                    message = f'У вас проверили работу «{lesson_title}»\n\n'
+                    if is_negative:
+                        status = 'К сожалению в работе нашлись ошибки\n\n'
+                    else:
+                        status = 'Преподателю всё понравилось, можно приступать к следущему уроку!\n\n'
 
-                response = requests.get(
-                    long_polling_url,
-                    headers=headers,
-                    params=params,
-                    timeout=120
-                )
-                response.raise_for_status()
-                logger.debug(f"Polling time = {start_time - time.time()}")
-
-                review_details = response.json()
-                timestamp = (review_details.get('last_attempt_timestamp')
-                             or review_details.get('timestamp_to_request'))
-
-                new_attempts = review_details.get('new_attempts')
-                if new_attempts:
-                    for attempt in new_attempts:
-                        lesson_title = attempt.get('lesson_title')
-                        lesson_url = attempt.get('lesson_url')
-                        is_negative = attempt.get('is_negative')
-
-                        message = f'У вас проверили работу «{lesson_title}»\n\n'
-                        if is_negative:
-                            status = 'К сожалению в работе нашлись ошибки\n\n'
-                        else:
-                            status = 'Преподателю всё понравилось, можно приступать к следующему уроку!\n\n'
-
-                        lesson_url_message = f'Ссылка на урок: {lesson_url}'
-                        message += status
-                        message += lesson_url_message
-                        telegram_message_sending_timestamp = time.time()
-                        try:
-                            await bot.send_message(
-                                chat_id=chat_id,
-                                text=message,
-                                parse_mode='Markdown',
-                            )
-                        except Exception as e:
-                            logger.debug(e)
-
-                        telegram_message_sent_timestamp = time.time()
-                        logger.info(
-                            f'Sent Telegram message. Duration: '
-                            f'{telegram_message_sent_timestamp - telegram_message_sending_timestamp}'
+                    lesson_url_message = f'Ссылка на урок: {lesson_url}'
+                    message += status
+                    message += lesson_url_message
+                    telegram_message_sending_timestamp = time.time()
+                    try:
+                        await bot.send_message(
+                            chat_id=chat_id,
+                            text=message,
+                            parse_mode='Markdown',
                         )
+                    except Exception as e:
+                        logger.debug(e)
 
-            except requests.exceptions.ConnectionError:
-                logger.error('Connection Error occurred. Sleeping for 120 seconds...')
-                await asyncio.sleep(120)
-            except requests.exceptions.ReadTimeout:
-                logger.debug('Request Timed Out')
-                continue
-            except requests.exceptions.HTTPError as err:
-                logger.debug(f'HTTP error. Status: {err}')
-            except Exception as e:
-                logger.debug(e)
-    finally:
+                    telegram_message_sent_timestamp = time.time()
+                    logger.debug(
+                        f'Send Telegram message. Duration: '
+                        f'{telegram_message_sent_timestamp - telegram_message_sending_timestamp}'
+                    )
+
+        except requests.exceptions.ConnectionError:
+            logger.error('Connection Error occurred. Sleeping for 120 seconds...')
+            time.sleep(120)
+        except requests.exceptions.ReadTimeout:
+            logger.error('Request Timed Out')
+            continue
+        except requests.exceptions.HTTPError as err:
+            logger.error(f'HTTP error. Status: {err}')
+        except Exception as e:
+            logger.error(e)
         await updater.stop()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     env = Env()
     env.read_env()
     auth_token = env.str('TOKEN')
